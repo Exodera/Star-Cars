2f4efb5b4e01577c5626f4a4668bcba9ab21eda7
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-11-07 19:49:58 +0200

    init commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='navbar'>
+      <h1>LOGO</h1>
+      <div className='nav-links first'>
+        <Link to = '/'>Model S</Link>
+        <Link to = '/'>Model S</Link>
+        <Link to = '/'>Model S</Link>
+        <Link to = '/'>Model S</Link>
+        <Link to = '/'>Solar Roof</Link>
+        <Link to = '/'>Solar Panels</Link>
+      </div>
+
+      <div className='nav-links last'>
+        <Link to = '/' className='shop'>Shop</Link>
+        <Link to = '/' className='account'>Account</Link>
+        <Link to = '/' className='menu'>Menu</Link>
+      </div>
+
+    </div>
+
+    </div>
+    
+  )
+}
+
+export default Header
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles/Home.css'
+import './styles/Global.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <div className='cont'>
+        Hello there my fellow mates
+        </div>
+    </div>
+  )
+}
+
+export default Home
